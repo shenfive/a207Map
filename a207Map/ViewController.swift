73f39517ad9d5ecd3666248460e46f06e961252e
Danny Shen <shenfive@gmail.com>
2020-09-12 14:14:26 +0800

    my Location

--- a207Map/ViewController.swift
+++ a207Map/ViewController.swift
@@ -14,7 +14,23 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            let latitude:CLLocationDegrees = 25.0443362
+            let longitude:CLLocationDegrees = 121.5135424
+            let location:CLLocationCoordinate2D =
+                CLLocationCoordinate2DMake(latitude, longitude)
+            let xScale:CLLocationDegrees = 0.001
+            let yScale:CLLocationDegrees = 0.001
+            
+            let span:MKCoordinateSpan =
+                MKCoordinateSpan(latitudeDelta: yScale, longitudeDelta: xScale)
+            let region:MKCoordinateRegion =
+                MKCoordinateRegion.init(center: location, span: span)
+            self.myMap.setRegion(region, animated: true)
+
+        }
+        
+        
     }
 
 
